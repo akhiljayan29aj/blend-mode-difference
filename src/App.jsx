afd88fb74dfbdf534d57201e647a8aefd7a3b7f6
akhiljayan29aj <akhiljayan29.aj@gmail.com>
2021-06-25 01:07:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <span className="text">Hello there, I am Akhil. Nice to meet you.</span>
+      </div>
+      <div className="white"></div>
+      <div className="black"></div>
+    </div>
+  );
+}
+
+export default App;
